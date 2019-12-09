8b84df64daab44f6042c6bdc7f2ca880447581a2
Mirko Lehmann <mirko.lehmann@comramo.de>
2019-12-09 15:26:30 +0100

    Add Day 1 Part 2

---
+++ 2019/day1_2.py
@@ -0,0 +1,34 @@
+#!/bin/env python3
+
+input = 'day1_1.input'
+
+total = 0
+
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
+
+with open(input) as f:
+    for mass in f:
+        fuel = int(mass) / 3
+        fuel = truncate(fuel)
+        fuel = fuel - 2
+        total += fuel
+        if fuel > 0:
+            while fuel > 0:
+                fuel = int(fuel) / 3
+                fuel = truncate(fuel)
+                fuel = fuel - 2
+                if fuel > 0:
+                    total += fuel
+                print(fuel)
+#            addedFuel = 1
+#            additionalFuel = 0
+#            while addedFuel > 0:
+#                moreFuel = int(finalFuel) / 3
+#                roundedMoreFuel = truncate(moreFuel)
+#                addedFuel = roundedMoreFuel - 2
+#                additionalFuel += addedFuel
+#                total += additionalFuel
+#                print(addedFuel)
+print(total)
